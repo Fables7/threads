72930c1d25787b9319f3d156f0219602bbf64bd0
Fabien Lestrat <129498777+Fables7@users.noreply.github.com>
2024-01-16 17:33:53 +0000

    Wrapped layout with clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,10 @@
 
+import { UserButton } from "@clerk/nextjs";
+ 
 export default function Home() {
   return (
-    <main>
-      <h1>Threads</h1>
-    </main>
-  );
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  )
 }
