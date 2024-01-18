1e30b5da2da08854d039c58828f1b9cbe8ea3038
Fabien Lestrat <129498777+Fables7@users.noreply.github.com>
2024-01-18 11:44:11 +0000

    Updated search page, created UserCard component, created fetchUsers action

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,9 +1,53 @@
 import React from "react";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import UserCard from "@/components/cards/UserCard";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import { profileTabs } from "@/constants";
+import Image from "next/image";
 
 const Page = async () => {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  // Fetch users
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
   return (
     <section>
       <h1 className="head-text mb-10">Search</h1>
+
+      {/* SearchBar */}
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No users</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
     </section>
   );
 };
